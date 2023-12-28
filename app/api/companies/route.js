e11be350d9b38765a934221d08b167b3f51ab37f
Connor Lane <connorlane@Connors-MacBook-Air.local>
2023-12-28 15:16:17 +0100

    for ash

--- app/api/companies/route.js
+++ app/api/companies/route.js
@@ -1,32 +1,64 @@
-import fs from 'fs';
-import path from 'path';
-import { NextRequest } from 'next/server';
+// pages/api/stock_info.js
+export default async function GET(req, res) {
+  // Extract parameters from the incoming request
+  const { ticker } = req.query;
 
+  // Call the Python script using child_process.spawn or exec
+  const spawn = require('child_process').spawn;
+  const process = spawn('python', ['./path/to/your/script.py', ticker]);
 
-export async function GET() {
-  // Load the JSON file
-  const jsonFilePath = path.join(process.cwd(), 'public/assets/data/ASX_List.json');
-  const fileContent = fs.readFileSync(jsonFilePath, 'utf8');
-  const companiesData = JSON.parse(fileContent);
-  const tickerList = companiesData.map(company => company.ASX_code);
-  const industryList = companiesData.map(company => company.GICsindustrygroup);
-  const price = companiesData.map(company => company.Price);
-  const change = companiesData.map(company => company["Change (%)"]);
+  // Collect data from script
+  let data = '';
+  for await (const chunk of process.stdout) {
+    data += chunk;
+  }
 
-  const responseData = {
-    companies: companiesData,
-    additional: tickerList,
-    industry: industryList,
-    price: price,
-    change: change
-  };
+  // Handle script completion
+  let error = '';
+  for await (const chunk of process.stderr) {
+    error += chunk;
+  }
 
-  return new Response(JSON.stringify(responseData), {
-    status: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
+  process.on('close', (code) => {
+    if (code !== 0) {
+      return res.status(500).json({ message: 'Error in Python script', error: error });
+    }
+    // Send back the result from Python script
+    res.status(200).json(JSON.parse(data));
   });
 }
+
+
+
+// import fs from 'fs';
+// import path from 'path';
+// import { NextRequest } from 'next/server';
+
+
+// export async function GET() {
+//   // Load the JSON file
+//   const jsonFilePath = path.join(process.cwd(), 'public/assets/data/ASX_List.json');
+//   const fileContent = fs.readFileSync(jsonFilePath, 'utf8');
+//   const companiesData = JSON.parse(fileContent);
+//   const tickerList = companiesData.map(company => company.ASX_code);
+//   const industryList = companiesData.map(company => company.GICsindustrygroup);
+//   const price = companiesData.map(company => company.Price);
+//   const change = companiesData.map(company => company["Change (%)"]);
+
+//   const responseData = {
+//     companies: companiesData,
+//     additional: tickerList,
+//     industry: industryList,
+//     price: price,
+//     change: change
+//   };
+
+//   return new Response(JSON.stringify(responseData), {
+//     status: 200,
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   });
+// }
 
 
