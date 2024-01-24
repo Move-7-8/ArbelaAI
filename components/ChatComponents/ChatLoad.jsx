167de9a265b55d5f0728f75bf5e3a0ac97a6d756
Ashlyn <ashlyn.bh@live.com.au>
2024-01-24 08:34:54 +0800

    ash24 Jan

--- components/ChatComponents/ChatLoad.jsx
+++ components/ChatComponents/ChatLoad.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
+import Lottie from 'react-lottie';
+import animationData from '../../public/assets/images/loading2.json'; // Path to your Lottie file
 
 const ChatLoad = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice' // Adjust as needed
+    }
+  };
+
   return (
-    <div className="flex flex-col w-full h-full max-h-screen rounded-lg border-blue-200 border-solid border-2 p-10">
-        <div className="flex flex-col h-full max-h-[calc(100vh-400px)] overflow-y-auto border-blue-200 border-solid border-2 p-6 rounded-lg">
-            <h2>Loading</h2>
+    <div className="flex flex-col items-center justify-center h-screen"> {/* Center the content vertically and horizontally */}
+      <div className="mt-20"> {/* Add margin to the top */}
+        {/* Lottie Animation Wrapper with Increased Opacity */}
+             <div style={{ 
+            opacity: 1, // Fully opaque
+            backgroundColor: '#fff', // Adding a white background (you can change the color as needed)
+            borderRadius: '10px' // Optional: to round the corners of the background
+          }}>
+          <Lottie options={defaultOptions} height={150} width={150} /> {/* Adjust the size if needed */}
         </div>
+      </div>
     </div>
   )
 }
 
-export default ChatLoad
+export default ChatLoad;
+
