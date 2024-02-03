01bb57b72cf6ae48ca24fcd12a90ba051e43c496
Connor Lane <connorlane@Connors-MacBook-Air.local>
2024-02-03 15:23:09 +0800

    connor 3 feb 330 pm

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -26,72 +26,50 @@ const Nav = () => {
 
   return (
     <nav className="w-full mb-16 pt-3 flex justify-between items-center">
-      {/* Logo and Brand Name */}
-      <Link href={session ? "/catalog" : "/"} className="flex gap-2 items-center">
-        <Image
-          src="/assets/images/logo3.png"
-          alt="Arbela Logo"
-          width={40}
-          height={40}
-          className="object-contain"
-        />
-        <p className="logo_text">Arbela</p>
-      </Link>
+    {/* Existing code omitted for brevity */}
+    <div> {/* This opening <div> seems to be missing in your snippet */}
+      {isCompanyPage && (
+        <CompanySearch />
+      )}
+      {/* Links and Profile */}
+      {session?.user ? (
+        <>
+          {/* Logged In User Links */}
+          <Link href="/catalog" className="nav_button">Catalog</Link>
+          {/* More links can be added here */}
 
-{/* Desktop Navigation  */}
-{/* For Logged In Users  */}
-{isCompanyPage && (
-  <CompanySearch />
-
-        )}
-
-        {/* Links and Profile */}
-        {session?.user ? (
-          <>
-            {/* Logged In User Links */}
-            <Link href="/catalog" className="nav_button">
-              Catalog
-            </Link>
-            {/* More links can be added here */}
-
-            {/* Profile and Dropdown */}
-            <div onClick={() => setToggleDropdown((prev) => !prev)} className="cursor-pointer">
-              <Image 
-                src={session?.user.image || "/assets/images/profile.png"}
-                width={37}
-                height={37}
-                className="rounded-full"
-                alt="profile"
-              />
-              {toggleDropdown && (
-                <div className="dropdown-menu">
-                  <Link href="/settings" className="dropdown-link">
-                    Settings
-                  </Link>
-                  <button
-                    type="button"
-                    className="dropdown-link"
-                    onClick={() => {
-                      setToggleDropdown(false);
-                      signOut({ callbackUrl: '/' });
-                    }}
-                  >
-                    Sign Out
-                  </button>
-                </div>
-              )}
-            </div>
-          </>
-        ) : (
-          // For Logged Out Users
-          <Link href="/catalog" passHref>
-            <button type="button" className="black_btn">
-              Sign Up || Sign In
-            </button>
-          </Link>
-        )}
-      </div>
-    </nav>
+          {/* Profile and Dropdown */}
+          <div onClick={() => setToggleDropdown((prev) => !prev)} className="cursor-pointer">
+            <Image
+              src={session?.user.image || "/assets/images/profile.png"}
+              width={37}
+              height={37}
+              className="rounded-full"
+              alt="profile"
+            />
+            {toggleDropdown && (
+              <div className="dropdown-menu">
+                <Link href="/settings" className="dropdown-link">Settings</Link>
+                <button
+                  type="button"
+                  className="dropdown-link"
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut({ callbackUrl: '/' });
+                  }}
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
+          </div>
+        </>
+      ) : (
+        // For Logged Out Users
+        <button type="button" className="black_btn" onClick={() => signIn()}>Sign Up || Sign In</button>
+      )}
+    </div> {/* Closing tag for the <div> wrapping session-related content */}
+  </nav>
   );
 }
 
