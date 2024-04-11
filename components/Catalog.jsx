ce2995d5b43d3710ca41f5babfd4af88bdbc1112
Connor Lane <connorlane@Connors-MacBook-Air.local>
2024-04-11 20:05:40 +0800

    connor catalog update

--- components/Catalog.jsx
+++ components/Catalog.jsx
@@ -4,6 +4,7 @@ import debounce from 'lodash.debounce';
 import StockCard from './StockCard';
 import { categoryMap } from '../constants';
 
+// CATALOG COMPONENT
 function Catalog({ searchText, selectedCategory, preloadedData, sortBy }) {
     const [tickers, setTickers] = useState(preloadedData || []);
     const [visibleCount, setVisibleCount] = useState(12);
